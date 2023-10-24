355252777dbe3ab8cbbbf75bb8bdec5d2626c402
Sergej <gorbatschewsergej@gmail.com>
2023-10-24 23:21:23 +0900

    Add code for button
    Added event listener to get new div no for grid.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,21 @@
 let squareNo; 
+let btn = document.querySelector("#new-grid");
 
+function getSquares() {
+   let tempNo = prompt("Enter the number of squares for the grid (max. 100)", 0);
+   if (tempNo > 100) {
+        while (tempNo > 100) {
+            tempNo = prompt("Enter the number of squares for the grid (max. 100)", 0);
+        }; 
+        squareNo = tempNo;
+    } else {
+        squareNo = tempNo;
+    }; 
+   console.log(squareNo);
+};
 
+btn.addEventListener("click", getSquares);
 
-for (let i = 0; i <= squareNo; i++) {
-    
-}
+// for (let i = 0; i <= squareNo; i++) {
+
+// }
