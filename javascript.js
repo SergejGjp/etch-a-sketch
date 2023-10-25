00f64cee78dd68764cc2fab9dbf13ac9fab4da8a
Sergej <gorbatschewsergej@gmail.com>
2023-10-25 22:06:56 +0900

    Add vertical grid maker
    Added function to make the vertical divs for the grid. Also added
    paragraph for for clicking the button.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 let squareNo; 
 let btn = document.querySelector("#new-grid");
+let div = document.querySelector(".container");
+let body = document.querySelector("body");
 
 function getSquares() {
    let tempNo = prompt("Enter the number of squares for the grid (max. 100)", 0);
@@ -12,10 +14,25 @@ function getSquares() {
         squareNo = tempNo;
     }; 
    console.log(squareNo);
+   const createBtn = document.createElement("button");
+   const para = document.createElement("p");
+   const brk = document.createElement("br");
+   para.textContent = `The grid will have a size of ${squareNo} squares.`;
+   createBtn.textContent = `Create a ${squareNo}x${squareNo} grid`;
+   body.insertBefore(para, div);
+   body.insertBefore(createBtn, div);
+   body.insertBefore(brk, div);
 };
 
 btn.addEventListener("click", getSquares);
 
-// for (let i = 0; i <= squareNo; i++) {
+function createVert(num) {
+    let pixel;
+    for (let i = 1; i<= num; i++) {
+        pixel = document.createElement("div");
+        pixel.setAttribute("class", "pixel");
+        div.appendChild(pixel);
+    };
+};
 
-// }
+
