1f9e3e777e5a12528d5db28f7f6e3ed625f75829
Sergej <gorbatschewsergej@gmail.com>
2023-11-07 22:26:04 +0900

    Change event listeners
    Made anonymous functions at event listeners to named functions.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 let gridSize = 16;
 const divContainer = document.querySelector("#container");
+let pixels = document.getElementsByClassName("pixel");
 
 function createGrid() {
     for (let i = 0; i < gridSize; i++) {
@@ -7,24 +8,27 @@ function createGrid() {
         for (let j = 0; j < gridSize; j++) {
             let box = document.createElement("div");
             box.classList.add("pixel");
+            box.addEventListener("mouseover", makeBlack);
             grid.appendChild(box);
         };
         divContainer.appendChild(grid);
-    };
-    let pixels = document.getElementsByClassName("pixel");
-    console.log(pixels.length);
-    for (let i = 0; i < pixels.length; i++) {
-        pixels[i].addEventListener("mouseover", () => pixels[i].classList.add("clicked"));
     };
 };
 
 createGrid();
 
+function makeBlack() {
+    this.classList.add("clicked");
+}
 
-
-
+function removeListener(array) {
+    for (let i = 0; i < array.length; i++) {
+        array[i].removeEventListener("mouseover", makeBlack);
+    };
+};
 
 const btn = document.querySelector("#new-grid");
+
 function getSquares() {
     let tempNo = prompt("Enter the number of squares for the grid (max. 100)", 0);
     if (tempNo > 100) {
@@ -47,3 +51,11 @@ function removeOldGrid(parent) {
 }
 
 btn.addEventListener("click", getSquares);
+
+let rgbBtn = document.querySelector("#rgb");
+
+function toRgbPointer() {
+    removeListener(pixels);
+};
+
+rgbBtn.addEventListener("click", toRgbPointer);
