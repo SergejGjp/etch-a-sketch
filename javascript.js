3d3e99dbe1c65466cb368217ec5092b1227e802f
Sergej <gorbatschewsergej@gmail.com>
2023-11-10 17:58:19 +0900

    Clean up and styling
    Moved some javascript code, added a color picker and eraser.
    Added some style.

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ function createGrid() {
         for (let j = 0; j < gridSize; j++) {
             let box = document.createElement("div");
             box.classList.add("pixel");
-            box.addEventListener("mouseover", makeBlack);
+            box.addEventListener("click", makeBlack);
             grid.appendChild(box);
         };
         divContainer.appendChild(grid);
@@ -16,6 +16,50 @@ function createGrid() {
 };
 
 createGrid();
+
+//function to create a new grid
+const btn = document.querySelector("#new-grid");
+
+function getSquares() {
+    let tempNo = parseInt(prompt("Enter the number of squares for the grid (max. 100)"));
+    if (tempNo == 0 || tempNo > 100 ) {
+        while (tempNo == 0 || tempNo > 100) {
+            tempNo = parseInt(prompt("Enter the number smaller or equal to 100"));
+            if (tempNo == null || Number.isNaN(tempNo)) {
+                tempNo = 16;
+            };
+            }; 
+        gridSize = tempNo;
+    } else if(Number.isNaN(tempNo) || tempNo == null) {
+        gridSize = 16;
+    } else {
+        gridSize = tempNo
+    }; 
+    console.log(gridSize);
+    removeOldGrid(divContainer);
+    createGrid();
+};
+
+function removeOldGrid(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    };
+}
+
+btn.addEventListener("click", getSquares);
+
+//functions to create and remove the event listeners
+function addListener(array, func) {
+    for (let i = 0; i < array.length; i++) {
+        array[i].addEventListener("click", func);
+    };
+};
+
+function removeListener(array, func) {
+    for (let i = 0; i < array.length; i++) {
+        array[i].removeEventListener("click", func);
+    };
+};
 
 function makeBlack() {
     this.classList.add("clicked");
@@ -39,53 +83,27 @@ function makeGrad() {
         break;
     };
     //console.log(color1);
+};
+
+function makeWhite() {
+    this.style.cssText = "background-color : white";
 }
 
-function addListener(array, func) {
-    for (let i = 0; i < array.length; i++) {
-        array[i].addEventListener("mouseover", func);
-    };
-};
-
-function removeListener(array, func) {
-    for (let i = 0; i < array.length; i++) {
-        array[i].removeEventListener("mouseover", func);
-    };
-};
-
-const btn = document.querySelector("#new-grid");
-
-function getSquares() {
-    let tempNo = prompt("Enter the number of squares for the grid (max. 100)", 0);
-    if (tempNo > 100) {
-         while (tempNo > 100) {
-             tempNo = prompt("Enter the number of squares for the grid (max. 100)", 0);
-         }; 
-         gridSize = tempNo;
-    } else {
-        gridSize = tempNo;
-    }; 
-    console.log(gridSize);
-    removeOldGrid(divContainer);
-    createGrid();
-};
-
-function removeOldGrid(parent) {
-    while(parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    };
+function makeColored() {
+    this.style.cssText = `background-color: ${colorPick.value}`;
 }
 
-btn.addEventListener("click", getSquares);
-
+//Here the codes makes the mouseover to the selected color or option
 let rgbBtn = document.querySelector("#rgb");
 
 function toRgbPointer() {
     color1 = 0;
     color2 = 0;
     color3 = 0;
+    removeListener(pixels, makeColored);
     removeListener(pixels, makeBlack);
     removeListener(pixels, makeGrad);
+    removeListener(pixels, makeWhite);
     addListener(pixels, makeRGB);
 };
 
@@ -95,9 +113,35 @@ let gradBtn = document.querySelector("#gradient");
 
 function toGradPointer() {
     color1 = 0;
+    removeListener(pixels, makeColored);
     removeListener(pixels, makeBlack);
     removeListener(pixels, makeRGB);
+    removeListener(pixels, makeWhite);
     addListener(pixels, makeGrad);
 }
 
 gradBtn.addEventListener("click", toGradPointer);
+
+let eraserBtn = document.querySelector("#eraser");
+
+function toEraser() {
+    removeListener(pixels, makeColored);
+    removeListener(pixels, toGradPointer);
+    removeListener(pixels, makeBlack);
+    removeListener(pixels, makeRGB);
+    addListener(pixels, makeWhite);
+}
+
+eraserBtn.addEventListener("click", toEraser);
+
+let colorPick = document.querySelector("#color");
+
+function toColor() {
+    removeListener(pixels, toGradPointer);
+    removeListener(pixels, makeBlack);
+    removeListener(pixels, makeRGB);
+    removeListener(pixels, makeWhite);
+    addListener(pixels, makeColored);
+}
+
+colorPick.addEventListener("click", toColor);
